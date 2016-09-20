1dabe118ec3bd054faf7d278d03dab03147a6415
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-20 21:02:33 +0200

    First version with template

--- index.php
+++ index.php
@@ -17,17 +17,6 @@ $query = $pdo->prepare('SELECT * FROM todos');
 
 $query->execute();
 
-//var_dump($query->fetchAll(PDO::FETCH_CLASS,Task::class));
+$tasks = $query->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,Task::class);
 
-//var_dump($query->fetchAll(PDO::FETCH_CLASS,'Task'));
-$results = $query->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,Task::class);
-$task1 = $results[0];
-
-var_dump($task1->id);
-var_dump($results[0]->id);
-
-var_dump($task1->description);
-var_dump($task1->completed);
-
-
-//require 'index.template.php';
+require 'index.template.php';
--- index.template.php
+++ index.template.php
@@ -9,8 +9,18 @@
 </head>
 <body>
 
-          <?= sum(1,2,3,5,100,546) ?>
+      <ul>
+          <?php foreach ($tasks as $task) : ?>
+              <li>
+                  <?php if ($task->completed) : ?>
+                      <strike><?= $task->description ?></strike>
+                  <?php else:?>
+                      <?= $task->description ?>
+                  <?php endif;?>
 
+              </li>
+          <?php endforeach;?>
+      </ul>
 
 </body>
 </html>
