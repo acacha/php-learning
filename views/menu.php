b990f6f988d0188d066f0222ad4189cf782037da
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-22 21:59:51 +0200

    solved bug in previous release

--- views/menu.php
+++ views/menu.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="">Tasques</a></li>
+        <li><a href="/">Tasques</a></li>
         <li><a href="persons">Persones</a></li>
         <li><a href="about">About</a></li>
         <li><a href="contact">Contact</a></li>
